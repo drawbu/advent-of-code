5050bbac4a82a0bee298aef09d49c51e26c4390c
drawbu <69208565+drawbu@users.noreply.github.com>
2022-12-06 09:14:05 +0100

    :sparkles: Add part 2 of day 6

--- day-6/main.go
+++ day-6/main.go
@@ -8,8 +8,9 @@ import (
 
 func main() {
 	fmt.Printf(
-		"part 1: %v\n",
+		"part 1: %v\npart 2: %v\n",
 		findStartOfPacketMarker(),
+		findStartOfMessageMarker(),
 	)
 }
 
@@ -20,7 +21,7 @@ func findStartOfPacketMarker() (result int) {
 	scanner := bufio.NewScanner(file)
 	if scanner.Scan() {
 		text := scanner.Text()
-		set := Set{}
+		set := Set{size: 4}
 		for i, c := range text {
 			if set.Add(c) {
 				return i + 1
@@ -41,20 +42,38 @@ func openFile(path string) *os.File {
 }
 
 type Set struct {
+	size  int
 	items []int32
 }
 
-func (d *Set) Add(element int32) bool {
-	d.items = append(d.items[len(d.items)/4:], element)
-	if len(d.items) < 4 {
+func (s *Set) Add(element int32) bool {
+	s.items = append(s.items[len(s.items)/s.size:], element)
+	if len(s.items) < s.size {
 		return false
 	}
-	for i, item := range d.items {
-		for _, item2 := range d.items[i+1:] {
+	for i, item := range s.items {
+		for _, item2 := range s.items[i+1:] {
 			if item == item2 {
 				return false
 			}
 		}
 	}
 	return true
+}
+
+func findStartOfMessageMarker() (result int) {
+	file := openFile("./day-6/input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan() {
+		text := scanner.Text()
+		set := Set{size: 14}
+		for i, c := range text {
+			if set.Add(c) {
+				return i + 1
+			}
+		}
+	}
+	return -1
 }
