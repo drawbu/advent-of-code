81306ee047f96eb586f7da891d947616af986a9d
Clément <clement2104.boillot@gmail.com>
2023-12-01 08:15:31 +0100

    Fix warnings

--- 2023/aoc.h
+++ 2023/aoc.h
@@ -3,7 +3,7 @@
 
     #include <stddef.h>
 
-    #define SOL(day_num, type, part_num) (solution_t){ \
+    #define SOL(day_num, type, part_num) { \
         .name = "day" #day_num "-" "part" #part_num, \
         .input = "./day" #day_num "/input_" #type ".txt", \
         .func = &(day ## day_num ## _part ## part_num), \
@@ -21,7 +21,7 @@ typedef struct {
 void day01_part1(char const *filename);
 void day01_part2(char const *filename);
 
-static solution_t SOLUTIONS[] = {
+static const solution_t SOLUTIONS[] = {
     DAY(01),
     { 0 },
 };
