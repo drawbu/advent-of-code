538422b2da4f880a20abdac73e494ecf0c84b1d8
Clément <clement2104.boillot@gmail.com>
2023-12-01 18:47:56 +0100

    Refactor solution

--- 2023/day01/source.c
+++ 2023/day01/source.c
@@ -12,38 +12,20 @@ void day01_part1(char const *filename)
     int sum = 0;
 
     READFILE(filename, line, {
-        int n;
         char *ptr = line;
         for (; !isdigit(*ptr) && *ptr != '\0'; ptr++);
         if (*ptr == '\0')
             continue;
-        n = *ptr - '0';
-        int tmp = 0;
-        for (size_t i = 0; line[i]; i++) {
-            if (!isdigit(line[i]))
-                continue;
-            tmp = line[i] - '0';
-        }
-        n = (n * 10) + tmp;
-        sum += n;
+        sum += (*ptr - '0') * 10;
+        for (ptr = line + strlen(line); !isdigit(*ptr); ptr--);
+        sum += *ptr - '0';
     })
     printf("%d\n", sum);
 }
 
-static const struct {
-    char *word;
-    int value;
-} LETTERS[] = {
-    { "one", 1 },
-    { "two", 2 },
-    { "three", 3 },
-    { "four", 4 },
-    { "five", 5 },
-    { "six", 6 },
-    { "seven", 7 },
-    { "eight", 8 },
-    { "nine", 9 },
-    { 0 },
+static const char *NUM[] = {
+    "one", "two", "three", "four", "five",
+    "six", "seven", "eight", "nine", NULL
 };
 
 static
@@ -51,11 +33,10 @@ int get_nbr(char const *str)
 {
     if (isdigit(*str))
         return *str - '0';
-    for (size_t i = 0; LETTERS[i].word != NULL; i++) {
-        if (strncmp(LETTERS[i].word, str, strlen(LETTERS[i].word)) == 0)
-            return LETTERS[i].value;
-    }
-    return INT_MAX;
+    for (int i = 0; NUM[i] != NULL; i++)
+        if (strncmp(NUM[i], str, strlen(NUM[i])) == 0)
+            return i + 1;
+    return 0;
 }
 
 void day01_part2(char const *filename)
@@ -64,27 +45,22 @@ void day01_part2(char const *filename)
     int sum = 0;
 
     READFILE(filename, line, {
-        int n;
-        char *ptr = line;
         int tmp = 0;
+        char *ptr = line;
         for (; *ptr != '\0'; ptr++) {
             tmp = get_nbr(ptr);
-            if (tmp != INT_MAX)
+            if (tmp != 0)
                 break;
         }
-        n = tmp;
         if (*ptr == '\0')
             continue;
-        tmp = 0;
-        int tmp2 = 0;
-        for (size_t i = 0; line[i]; i++) {
-            tmp2 = get_nbr(line + i);
-            if (tmp2 == INT_MAX)
-                continue;
-            tmp = tmp2;
+        sum += tmp * 10;
+        for (ptr = line + strlen(line); ptr + 1 != line; ptr--) {
+            tmp = get_nbr(ptr);
+            if (tmp != 0)
+                break;
         }
-        n = (n * 10) + tmp;
-        sum += n;
+        sum += tmp;
     })
     printf("%d\n", sum);
 }
