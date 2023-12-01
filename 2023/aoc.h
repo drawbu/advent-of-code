8284266eea6103a7b9260467621c2833511da86c
Clément <clement2104.boillot@gmail.com>
2023-12-01 06:43:03 +0100

    Add day 01 of 2023

--- 2023/aoc.h
+++ 2023/aoc.h
@@ -6,23 +6,23 @@
     #define SOL(day_num, type, part_num) (solution_t){ \
         .name = "day" #day_num "-" "part" #part_num, \
         .input = "./day" #day_num "/input_" #type ".txt", \
-        .func = &(day ## day_num ## part ## part_num), \
+        .func = &(day ## day_num ## _part ## part_num), \
     }
     #define DAY(day_num) \
-        SOL(day_num, test, 1), \
         SOL(day_num, full, 1), \
-        SOL(day_num, test, 2), \
         SOL(day_num, full, 2)
 
 typedef struct {
     char *name;
     char *input;
-    void (*func)(char *);
+    void (*func)(char const *);
 } solution_t;
 
-void foo(char *);
+void day01_part1(char const *filename);
+void day01_part2(char const *filename);
 
 static solution_t SOLUTIONS[] = {
+    DAY(01),
     { 0 },
 };
 
---
+++ 2023/day01/source.c
@@ -0,0 +1,94 @@
+#include <ctype.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "aoc.h"
+
+void day01_part1(char const *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    char *line;
+    size_t len = 0;
+    int sum = 0;
+
+    while (getline(&line, &len, fp) != -1) {
+        int n;
+        char *ptr = line;
+        for (; !isdigit(*ptr) && *ptr != '\0'; ptr++);
+        if (*ptr == '\0')
+            continue;
+        n = *ptr - '0';
+        int tmp = 0;
+        for (size_t i = 0; line[i]; i++) {
+            if (!isdigit(line[i]))
+                continue;
+            tmp = line[i] - '0';
+        }
+        n = (n * 10) + tmp;
+        sum += n;
+    }
+    printf("%d\n", sum);
+}
+
+static const struct {
+    char *word;
+    int value;
+} LETTERS[] = {
+    { "one", 1 },
+    { "two", 2 },
+    { "three", 3 },
+    { "four", 4 },
+    { "five", 5 },
+    { "six", 6 },
+    { "seven", 7 },
+    { "eight", 8 },
+    { "nine", 9 },
+    { 0 },
+};
+
+static
+int get_nbr(char const *str)
+{
+    if (isdigit(*str))
+        return *str - '0';
+    for (size_t i = 0; LETTERS[i].word != NULL; i++) {
+        if (strncmp(LETTERS[i].word, str, strlen(LETTERS[i].word)) == 0)
+            return LETTERS[i].value;
+    }
+    return INT_MAX;
+}
+
+void day01_part2(char const *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    char *line;
+    size_t len = 0;
+    int sum = 0;
+
+    while (getline(&line, &len, fp) != -1) {
+        int n;
+        char *ptr = line;
+        int tmp = 0;
+        for (; *ptr != '\0'; ptr++) {
+            tmp = get_nbr(ptr);
+            if (tmp != INT_MAX)
+                break;
+        }
+        n = tmp;
+        if (*ptr == '\0')
+            continue;
+        tmp = 0;
+        int tmp2 = 0;
+        for (size_t i = 0; line[i]; i++) {
+            tmp2 = get_nbr(line + i);
+            if (tmp2 == INT_MAX)
+                continue;
+            tmp = tmp2;
+        }
+        n = (n * 10) + tmp;
+        sum += n;
+    }
+    printf("%d\n", sum);
+}
