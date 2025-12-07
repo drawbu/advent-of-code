3f0932930bb5bed0d98c38ab2f2794f772b7f9dd
Clément <git@drawbu.dev>
2025-12-07 09:18:05 -0800

    2025: improve day 07

--- 2025/src/day07.zig
+++ 2025/src/day07.zig
@@ -1,81 +1,41 @@
 const std = @import("std");
 const utils = @import("utils.zig");
 
-const TileName = enum(u2) {
-    pipe,
-    split,
-    start,
-    empty,
-};
-const Tile = union(TileName) {
-    pipe: u62,
-    split: void,
-    start: void,
-    empty: void,
-};
-
-fn set_char(input: [][]Tile, y: usize, x: usize, value: u62) usize {
-    switch (input[y][x]) {
-        .pipe => |*pipe| input[y][x] = .{ .pipe = pipe.* + value },
-        .start => unreachable,
-        .split => {
-            if (x != 0)
-                _ = set_char(input, y, x - 1, value);
-            if (x != input[0].len - 1)
-                _ = set_char(input, y, x + 1, value);
-            return 1;
-        },
-        .empty => input[y][x] = .{ .pipe = value },
-    }
-    return 0;
-}
-
 pub fn solution(alloc: std.mem.Allocator, file_content: []const u8) !utils.AOCSolution {
     var sol = utils.AOCSolution{ .part1 = 0, .part2 = 0 };
     var input = try utils.AOCInput.init(alloc, file_content);
     defer input.deinit();
 
-    const grid = blk: {
-        var buf: std.ArrayList([]Tile) = .empty;
-        for (input.items) |line| {
-            var tmp: std.ArrayList(Tile) = .empty;
-            for (line) |c| {
-                switch (c) {
-                    '|' => try tmp.append(alloc, .{ .pipe = 0 }),
-                    'S' => try tmp.append(alloc, .start),
-                    '^' => try tmp.append(alloc, .split),
-                    '.' => try tmp.append(alloc, .empty),
-                    else => unreachable,
+    // (c >> 3) & 3 =>
+    // 'S'=1   '.'=0   '^'=1 << 63
+    const lookup = [_]usize{ undefined, 0, 1, 1 << 63 };
+
+    var buf = [_]usize{0} ** 300;
+    const width = input.items[0].len;
+    const line1 = buf[0..width];
+    const line2 = buf[width .. width * 2];
+
+    for (input.items[0], line2) |c, *r| r.* = lookup[(c >> 3) & 3];
+    for (input.items[1..]) |rline2| {
+        std.mem.copyForwards(usize, line1, line2);
+        for (rline2, line2) |c, *r| r.* = lookup[(c >> 3) & 3];
+
+        for (line1, 0..) |c, x| {
+            if (c != 1 << 63 and c > 0) {
+                if (line2[x] == 1 << 63) {
+                    line2[x - 1] += c;
+                    line2[x + 1] += c;
+                    sol.part1 += 1;
+                } else {
+                    line2[x] += c;
                 }
             }
-            try buf.append(alloc, try tmp.toOwnedSlice(alloc));
-        }
-        break :blk try buf.toOwnedSlice(alloc);
-    };
-    defer {
-        for (grid) |l| alloc.free(l);
-        alloc.free(grid);
-    }
-
-    for (grid, 0..) |line, y| {
-        for (line, 0..) |c, x| {
-            switch (c) {
-                .pipe => |*pipe| {
-                    if (y != grid.len - 1)
-                        sol.part1 += set_char(grid, y + 1, x, pipe.*);
-                },
-                .start => sol.part1 += set_char(grid, y + 1, x, 1),
-                .empty => {},
-                .split => {},
-            }
         }
     }
 
-    for (grid[grid.len - 2]) |c| {
-        switch (c) {
-            .pipe => |*pipe| sol.part2 += pipe.*,
-            else => {},
-        }
+    for (line2) |tile| {
+        if (tile != 1 << 63)
+            sol.part2 += tile;
     }
     return sol;
 }
@@ -102,7 +62,7 @@ test "test input" {
     const sol = try solution(std.testing.allocator, test_input);
 
     try std.testing.expectEqual(21, sol.part1);
-    // try std.testing.expectEqual(40, sol.part2);
+    try std.testing.expectEqual(40, sol.part2);
 }
 
 test "actual input" {
